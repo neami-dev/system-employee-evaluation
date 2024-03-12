b67e3eee662553987f76d3174468be0aefa0dfdd
abdelkabir <abdkabirneami@gmail.com>
2024-03-12 14:36:46 +0000

    create folder action and create methods

--- app/employee/profile/page.js
+++ app/employee/profile/page.js
@@ -5,6 +5,7 @@ import { auth } from "../../../firebase/firebase-config";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import getDocument from "@/firebase/firestore/getDocument";
+import { folders, getTasks, getuser, lists, spaces } from "@/app/actions/action";
 
 export default function page() {
     const [userData, setUserData] = useState({});
@@ -14,9 +15,6 @@ export default function page() {
     const route = useRouter();
     const infoDoc = { collectionName: "userData", id: data?.uid };
 
-    const logout = async () => {
-        await signOut(auth);
-    };
     // get info the user score department ...
     const getInfo = async () => {
         if (infoDoc.id !== undefined && infoDoc.collectionName !== undefined) {
@@ -26,7 +24,31 @@ export default function page() {
             );
             setUserData(result.result.data());
         }
+        const data = await getTasks();
+        console.log(data);
+        await getuser();
+        lists().then((res) => {
+            console.log(res);
+        });
     };
+
+    // const getTasks = async ()=>{
+    //     "use server"
+    //     const listId = "901502971367";
+    //     const resp = await fetch(
+    //         `https://api.clickup.com/api/v2/list/${listId}/task?assignees[]=62621353`,
+    //         {
+    //             method: "GET",
+    //             headers: {
+    //                 Authorization: auth,
+    //             },
+    //         }
+    //     );
+
+    //     const data = await resp.json();
+    //     console.log("data",data);
+
+    // }
 
     // const testGetDocuments  = async ()=>{
     //     const result2 = await getDocuments("task");
@@ -54,25 +76,22 @@ export default function page() {
     console.log(userData);
     return (
         <>
-            <div>page</div>
-            <Link className=" bg-lime-700" href="/auth/loginPage">
-                login
-            </Link>
-            <button className=" bg-red-700" onClick={logout}>
-                logout
-            </button>
-            <h4>{data?.email}</h4>
-            <ul>
-                <li>department {userData?.department}</li>
-                <li>score {userData?.score}</li>
-                <li>joiningDate {userData.joiningDate?.nanoseconds}</li>
+            <div className=" mt-10">
+                <div>page</div>
+
+                <h4>{data?.email}</h4>
                 <ul>
-                    skills{" "}
-                    {userData?.skills?.map((skill, index) => {
-                        return <li key={index}>{skill}</li>;
-                    })}
+                    <li>department {userData?.department}</li>
+                    <li>score {userData?.score}</li>
+                    <li>joiningDate {userData.joiningDate?.nanoseconds}</li>
+                    <ul>
+                        skills{" "}
+                        {userData?.skills?.map((skill, index) => {
+                            return <li key={index}>{skill}</li>;
+                        })}
+                    </ul>
                 </ul>
-            </ul>
+            </div>
         </>
     );
 }
