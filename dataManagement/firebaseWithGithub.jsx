d29834c64dbbebb18476f936e834fffdf53614cd
abdelkabir <abdkabirneami@gmail.com>
2024-03-21 23:22:22 +0000

    added function firebaseWithGithub

--- dataManagement/firebaseWithGithub.jsx
+++ dataManagement/firebaseWithGithub.jsx
@@ -1,18 +1,31 @@
 import { getTotalCommitsForToday } from "@/app/api/actions/githubActions";
+import { auth } from "@/firebase/firebase-config";
+import getDocument from "@/firebase/firestore/getDocument";
 import updateDocument from "@/firebase/firestore/updateDocument";
 
 export async function firebaseWithGithub() {
-    const GithubTotalCommits = await getTotalCommitsForToday();
-    console.log("GithubTotalCommits", GithubTotalCommits);
-    if (GithubTotalCommits !== 1) {
+    const id = auth?.currentUser?.uid;
+    console.log(id);
+    if(id !==undefined){
+        const response = await getDocument(
+            "userData",
+            id
+        );
+    const totalCommits = response.result.data()?.commits;
+    console.log("totalCommits",totalCommits);
+    const githubTotalCommits = await getTotalCommitsForToday();
+    console.log("githubTotalCommits",githubTotalCommits);
+
+    if (totalCommits !== githubTotalCommits) {
         const response = await updateDocument({
             collectionName: "userData",
-            id: "xRcfN17VH6YMHRK8dceKd3WURgM2",
-            data: { score: 90 },
+            id:id,
+            data: { commits: githubTotalCommits },
         });
-        console.log("result===", response.result);
         console.log("error===", response.error);
     }
 
-    return <div>firebaseWithGithub</div>;
+    }
+   
+
 }
