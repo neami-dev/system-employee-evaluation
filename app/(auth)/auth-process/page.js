42525e098a4d22c161441aaf8527b330dca05974
cleanLine <88499015+asbar51@users.noreply.github.com>
2024-03-25 13:38:53 +0000

    add check if tokens are stored .

--- app/(auth)/auth-process/page.js
+++ app/(auth)/auth-process/page.js
@@ -1,22 +1,28 @@
 "use client"
-import { GetTokenfirebaseClickup } from "@/dataManagement/firebaseWIthClickup";
+import Loading from "@/components/component/Loading";
+import { GetTokenfirebaseClickup } from "@/dataManagement/firebaseClickup/GetTokenfirebaseClickup";
+import { GetTokenFirebaseGithub } from "@/dataManagement/firebaseWithGithub/GetTokenFIrebaseGithub";
 import { auth } from "@/firebase/firebase-config";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const IntegrationPage = () => {
     const route = useRouter();
-    const [Clickup, setClickup] = useState(false)
+    const [Clickup, setClickup] = useState(null)
+    const [Github, setGithub] = useState(null)
 
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
             GetTokenfirebaseClickup(setClickup,user?.uid)
+            GetTokenFirebaseGithub(setGithub,user?.uid)
         });
     
     }, [])
     
 
     return (
+        <>
+        {Clickup == null && Github == null ? <Loading /> :
         <div className="min-h-screen bg-blue-50 flex flex-col items-center justify-center p-4 lg:p-10">
             <h1 className="text-2xl lg:text-4xl font-bold text-blue-900 mb-4 lg:mb-6">Service Integration Setup</h1>
             <p className="text-md lg:text-lg text-blue-800 mb-8 lg:mb-10 text-center">
@@ -31,7 +37,7 @@ const IntegrationPage = () => {
                     `https://app.clickup.com/api?client_id=${process.env.NEXT_PUBLIC_CLICKUP_CLIENT_ID}&redirect_uri=http://localhost:3000/api/clickupAuth`
                         )
                 }
-                    className={`${Clickup ? "bg-green-500 hover:bg-green-700": "bg-blue-500 hover:bg-blue-700"}bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out flex items-center space-x-2`}>
+                    className={`${Clickup ? "bg-green-500 hover:bg-green-700": "bg-blue-500 hover:bg-blue-700"} text-white font-bold py-3 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out flex items-center space-x-2`}>
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"></path></svg>
                         <span>ClickUp</span>
                 </button>
@@ -44,7 +50,7 @@ const IntegrationPage = () => {
                 <button onClick={() => route.push(
                     `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&scope=user,repo`
                     )} 
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out flex items-center space-x-2">
+                    className={`${Github ? "bg-green-500 hover:bg-green-700": "bg-blue-500 hover:bg-blue-700"} text-white font-bold py-3 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out flex items-center space-x-2`}>
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.483 0-.237-.008-.866-.013-1.7-2.782.605-3.369-1.343-3.369-1.343-.454-1.155-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.031 1.531 1.031.892 1.529 2.341 1.087 2.912.832.092-.647.35-1.087.636-1.337-2.22-.253-4.555-1.111-4.555-4.941 0-1.091.39-1.983 1.029-2.682-.103-.253-.446-1.272.098-2.65 0 0 .84-.269 2.75 1.026A9.564 9.564 0 0112 6.802c.85.004 1.705.115 2.504.337 1.909-1.295 2.747-1.026 2.747-1.026.546 1.378.203 2.397.1 2.65.64.699 1.028 1.591 1.028 2.682 0 3.841-2.339 4.685-4.566 4.931.359.309.678.921.678 1.859 0 1.341-.012 2.421-.012 2.749 0 .269.18.579.688.482A10.001 10.001 0 0022 12c0-5.523-4.477-10-10-10z"></path></svg>
                         <span>GitHub</span>
                 </button>
@@ -60,6 +66,8 @@ const IntegrationPage = () => {
                 </button>
             </div>
         </div>
+    }
+    </>
     );
 };
 
