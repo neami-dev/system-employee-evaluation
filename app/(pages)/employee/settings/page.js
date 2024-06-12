77e1d01a469082d7f24211e4caa4a56527582b5d
abdelkabir <abdkabirneami@gmail.com>
2024-06-12 09:43:00 +0100

    add link

--- app/(pages)/employee/settings/page.js
+++ app/(pages)/employee/settings/page.js
@@ -363,7 +363,7 @@ export default function page() {
                     <Button
                         onClick={() =>
                             route.push(
-                                `https://app.clickup.com/api?client_id=${process.env.NEXT_PUBLIC_CLICKUP_CLIENT_ID}&redirect_uri=http://localhost:3000/api_services/clickupAuth`
+                                `https://app.clickup.com/api?client_id=${process.env.NEXT_PUBLIC_CLICKUP_CLIENT_ID}&redirect_uri=https://system-employee-evaluation.vercel.app/api_services/clickupAuth`
                             )
                         }
                         className="mt-6 w-[160px] gap-x-3 "
